58eeca9fd7d5629af58ca391885e4d8a6aa97108
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2014-04-17 09:54:05 -0500

    Allow Authorization header in CORS.

--- src/main/java/info/agrueneberg/fhir/filters/CorsFilter.java
+++ src/main/java/info/agrueneberg/fhir/filters/CorsFilter.java
@@ -18,7 +18,7 @@ public class CorsFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, content-type");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, content-type, authorization");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Expose-Headers", "location");
         chain.doFilter(req, res);
