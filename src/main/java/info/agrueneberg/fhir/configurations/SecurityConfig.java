3b0aab960fd6d046e4afc85a20f12737de2162bf
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2014-04-17 09:51:04 -0500

    Disable security for OPTIONS requests.

--- src/main/java/info/agrueneberg/fhir/configurations/SecurityConfig.java
+++ src/main/java/info/agrueneberg/fhir/configurations/SecurityConfig.java
@@ -3,6 +3,7 @@ package info.agrueneberg.fhir.configurations;
 import info.agrueneberg.fhir.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -25,6 +26,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
             .httpBasic();
