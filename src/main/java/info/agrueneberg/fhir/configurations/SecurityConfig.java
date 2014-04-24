1e5755311114dbdcb57f111769b1558d8e698414
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2014-04-24 15:04:42 -0500

    Disable CSRF to allow API inserts and updates.

--- src/main/java/info/agrueneberg/fhir/configurations/SecurityConfig.java
+++ src/main/java/info/agrueneberg/fhir/configurations/SecurityConfig.java
@@ -25,6 +25,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+            .csrf().disable()
             .authorizeRequests()
                 .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .anyRequest().authenticated()
