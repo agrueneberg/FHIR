c86ce4f2c1a21be4dd4b96774335efb8929f81c9
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2014-05-02 16:02:08 -0500

    Use @Repository annotation on ResourceRepository.

--- src/main/java/info/agrueneberg/fhir/repositories/ResourceRepository.java
+++ src/main/java/info/agrueneberg/fhir/repositories/ResourceRepository.java
@@ -13,9 +13,9 @@ import java.util.Map;
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Repository;
 
-@Service
+@Repository
 public class ResourceRepository {
 
     private final DBCollection resourcesCollection;
